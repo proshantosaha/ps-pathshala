91b74e8086bdfbd6afcc4ae5f4578536355096d1
proshantosaha <proshantoll71@gmail.com>
2025-02-21 23:44:45 +0600

    home page create

--- queries/courses.js
+++ queries/courses.js
@@ -1,13 +1,22 @@
 import { Course } from "@/model/course-model";
 import { Category } from "@/model/category-model";
-
-import { model } from "mongoose";
 import { User } from "@/model/user-model";
 import { Testimonial } from "@/model/testimonial-model";
 import { Module } from "@/model/module-model";
 
-export async function getCourses() {
+import { replaceMongoIdInArray } from "@/lib/convertData";
+
+export async function getCourseList() {
   const courses = await Course.find({})
+    .select([
+      "title",
+      "subtitle",
+      "thumbnail",
+      "modules",
+      "price",
+      "category",
+      "instructor",
+    ])
     .populate({
       path: "category",
       model: Category,
@@ -23,6 +32,7 @@ export async function getCourses() {
     .populate({
       path: "modules",
       model: Module,
-    });
-  return courses;
+    })
+    .lean();
+  return replaceMongoIdInArray(courses);
 }
