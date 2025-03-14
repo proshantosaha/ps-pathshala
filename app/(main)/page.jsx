6b6552ceabdab0b495e8c1c7ed4d7b74fdfd8dda
proshantosaha <proshantoll71@gmail.com>
2025-03-14 23:54:15 +0600

    some chemges

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -85,12 +85,12 @@ const HomePage = async () => {
                 className="relative overflow-hidden rounded-lg border bg-background p-2 hover:scale-105 transition-all duration-500 ease-in-out"
               >
                 <div className="flex  flex-col gap-4 items-center justify-between rounded-md p-6">
-                  <Image
+                  {/* <Image
                     src={`/assets/images/categories/${category.thumbnail}`}
                     alt={category.title}
                     width={100}
                     height={100}
-                  />
+                  /> */}
                   <h3 className="font-bold">{category.title}</h3>
                 </div>
               </Link>
