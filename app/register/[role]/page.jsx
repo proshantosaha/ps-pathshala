e182412d9c4f6849bde465dd26db0f72bc67e52f
proshantosaha <proshantoll71@gmail.com>
2025-03-21 23:42:35 +0600

    register usr completed

--- app/register/[role]/page.jsx
+++ app/register/[role]/page.jsx
@@ -1,10 +1,11 @@
 import { SignupForm } from "../_components/signup-form";
 
-const RegisterPage = () => {
+const RegisterPage = async ({ params }) => {
+  const { role } = await params;
   return (
     <div className="w-full flex-col h-screen flex items-center justify-center">
       <div className="container">
-        <SignupForm />
+        <SignupForm role={role} />
       </div>
     </div>
   );
