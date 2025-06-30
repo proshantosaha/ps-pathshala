9253f8d2f0f272b5b2603ab399f8858852a02ef1
proshantosaha <proshantoll71@gmail.com>
2025-06-30 23:47:13 +0600

    workin for quiz set

--- app/dashboard/quiz-sets/[quizSetId]/page.jsx
+++ app/dashboard/quiz-sets/[quizSetId]/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+
 import AlertBanner from "@/components/alert-banner";
 import { IconBadge } from "@/components/icon-badge";
 import { LayoutDashboard } from "lucide-react";
@@ -6,61 +6,34 @@ import { QuizSetAction } from "./_components/quiz-set-action";
 import { TitleForm } from "./_components/title-form";
 import { AddQuizForm } from "./_components/add-quiz-form";
 import { cn } from "@/lib/utils";
-import { useState } from "react";
 import { Pencil } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Delete } from "lucide-react";
 import { Trash } from "lucide-react";
 import { CircleCheck } from "lucide-react";
 import { Circle } from "lucide-react";
-const initialQuizes = [
-  {
-    id: 1,
-    title: "What is HTML ?",
-    options: [
-      {
-        label: "A programming language",
-        isTrue: false,
-      },
-      {
-        label: "A markup language",
-        isTrue: true,
-      },
-      {
-        label: "A famous book",
-        isTrue: false,
-      },
-      {
-        label: "A famous tv show",
-        isTrue: false,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "What is Javascript ?",
-    options: [
-      {
-        label: "A programming language",
-        isTrue: true,
-      },
-      {
-        label: "A markup language",
-        isTrue: false,
-      },
-      {
-        label: "A famous book",
-        isTrue: false,
-      },
-      {
-        label: "A famous tv show",
-        isTrue: false,
-      },
-    ],
-  },
-];
-const EditQuizSet = () => {
-  const [quizes, setQuizes] = useState(initialQuizes);
+import { QuizCardActions } from "./_components/quiz-card-actions";
+import { getQuizSetById } from "@/queries/quizzes";
+
+
+
+const EditQuizSet = async ({params: {quizSetId}}) => {
+  const quizSet = await getQuizSetById(quizSetId);
+
+
+  const quizzes = quizSet.quizIds.map(quiz => {
+    return {
+      id: quiz._id.toString(),
+      title: quiz.title,
+      options: quiz.options.map(option => {
+        return {
+          label: option.text,
+          isTrue: option.is_correct
+        }
+      })
+    }
+  })
+
   return (
     <>
       <AlertBanner
@@ -75,13 +48,13 @@ const EditQuizSet = () => {
           {/* Quiz List */}
           <div className="max-lg:order-2">
             <h2 className="text-xl mb-6">Quiz List</h2>
-            <AlertBanner
+            {quizzes?.length === 0 && (<AlertBanner
               label="No Quiz are in the set, add some using the form above."
               variant="warning"
               className="rounded mb-6"
-            />
+            />)}
             <div className="space-y-6">
-              {quizes.map((quiz) => {
+              {quizzes?.map((quiz) => {
                 return (
                   <div
                     key={quiz.id}
@@ -90,7 +63,7 @@ const EditQuizSet = () => {
                     <h2 className="mb-3">{quiz.title}</h2>
 
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
-                      {quiz.options.map((option) => {
+                      {quiz.options?.map((option) => {
                         return (
                           <div
                             className={cn(
@@ -110,16 +83,7 @@ const EditQuizSet = () => {
                       })}
                     </div>
                     <div className="flex items-center justify-end gap-2 mt-6">
-                      <Button variant="ghost" size="sm">
-                        <Pencil className="w-3 mr-1" /> Edit
-                      </Button>
-                      <Button
-                        size="sm"
-                        className="text-destructive"
-                        variant="ghost"
-                      >
-                        <Trash className="w-3 mr-1" /> Delete
-                      </Button>
+                      <QuizCardActions quiz={quiz} quizSetId={quizSetId}/>
                     </div>
                   </div>
                 );
@@ -134,13 +98,14 @@ const EditQuizSet = () => {
             <div className="max-w-[800px]">
               <TitleForm
                 initialData={{
-                  title: "Reactive Accelerator",
+                  title: quizSet.title,
                 }}
+                quizSetId={quizSet.id}
               />
             </div>
 
             <div className="max-w-[800px]">
-              <AddQuizForm setQuizes={setQuizes} />
+              <AddQuizForm quizSetId={quizSetId}/>
             </div>
           </div>
         </div>
